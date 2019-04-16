79446584203c563442ba1b10c39e3db035d1cfac
Eric Zong <ericzonglu@126.com>
2019-04-16 14:45:26 +0800

    Add filter options

--- git-all.ps1
+++ git-all.ps1
@@ -1,4 +1,4 @@
-#v1.0.5
+#v1.1.0
 <#
 .Synopsis
     Batch operate the Git repositories under the specified folder
@@ -10,13 +10,17 @@
     git-all push
 .EXAMPLE
     git-all pull D:\parent\of\git\dirs
+.EXAMPLE
+    git-all -include test.* -exclude pro.*
 #>
 param(
     [Parameter(Position=0)]
     [ValidateScript({$_ -eq "pull" -or $_ -eq "push"})]
     [string]$type="pull",
     [Parameter(Position=1)]
-    [string]$dir="."
+    [string]$dir=".",
+    [string[]]$include,
+    [string[]]$exclude
 )
 
 $currentPath=$PWD
@@ -39,21 +43,53 @@ foreach($d in dir $dir -Directory)
 {
     if(-not (Test-Path (Join-Path $d.FullName ".git")))
     {
-        Write-Warning "Skip non-git project" ($d.Name) "..."
+        Write-Warning ("Skip non-git project $($d.Name)...")
+        continue
+    }
+
+    if(!(isInclude $d $include $exclude))
+    {
+        Write-Warning "Filter out $($d.Name)" 
         continue
     }
 
     cd $d.FullName
     if($type -eq "pull")
     {
-        Write-Host "Updating project" ($d.Name) "..."
+        Write-Host "Updating project $($d.Name)..."
         git.exe pull --progress
     }
     else
     {
-        Write-Host "Push project" ($d.Name) "..."
+        Write-Host "Push project $($d.Name)..."
         git.exe push --progress
     }
 }
 
 cd $currentPath
+
+function matchArray([string]$name, [string[]]$array)
+{
+    foreach($i in $array)
+    {
+        if($name -match $i)
+        {
+            return $true
+        }
+    }
+}
+
+function isInclude([string]$name, [string[]]$include, [string[]]$exclude) 
+{
+    if($exclude -and (matchArray $name $exclude))
+    {
+        return $false
+    }
+
+    if($include -and !(matchArray $name $include))
+    {
+        return $false
+    }
+
+    return $true
+}
