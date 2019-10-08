991ae532cac5625fdce4a3714255e25a4bd82c36
Eric Zong <ericzonglu@126.com>
2019-10-08 23:20:45 +0800

    install jekyll script

---
+++ bat/jekyll/install-jekyll.ps1
@@ -0,0 +1,10 @@
+scoop install ruby
+# ruby -v
+scoop install msys2
+msys2
+ridk install
+# gem -v
+gem install jekyll bundler
+gem install jekyll-paginate
+gem install jekyll-feed
+gem install jemoji
