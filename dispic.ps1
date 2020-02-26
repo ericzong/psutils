ccb9ca04e6bb6eac82d1ddd7137501c08294a606
Eric Zong <ericzonglu@126.com>
2020-02-26 16:36:03 +0800

    增加脚本说明

--- dispic.ps1
+++ dispic.ps1
@@ -1,3 +1,15 @@
+#v1.0.0
+<#
+功能：分拣横向/纵向的图片。
+参数：
+  src - 需分拣的图片所在文件夹（不递归处理图片），默认为当前工作目录。
+  hDir - 横向图片分拣到的文件夹，在 src 下，默认为 h。
+  vDir - 纵向图片分拣到的文件夹，在 src 下，默认为 v。
+  limit - 处理限制，图片最长边像素大于该值才进行分拣，默认为 1920。
+输出：符合限制的横向/纵向图片分别拷贝到 hDir/vDir。
+依赖：System.Drawing
+#>
+
 [CmdletBinding()]
 param(
   [Parameter()]
@@ -5,14 +17,16 @@ param(
   [Parameter()]
   [string]$hDir = "h",
   [Parameter()]
-  [string]$vDir = "v"
+  [string]$vDir = "v",
+  [Parameter()]
+  [int32]$limit = 1920
 )
 # 数据预处理
 $exts = @('.jpg', '.jpeg', '.png')
 $src = Resolve-Path $src
 $hDir = [System.IO.Path]::Combine($src, $hDir)
 $vDir = [System.IO.Path]::Combine($src, $vDir)
-# 库引加载
+# 引用库加载
 [system.reflection.assembly]::loadwithpartialname("System.Drawing") | Out-Null
 # 环境预处理
 if(![System.IO.Directory]::Exists($hDir)) {
@@ -39,8 +53,7 @@ $list = Get-ChildItem $src -File |
       $toDir = $vDir
     }
 
-    if($max -gt 1920) {
+    if($max -gt $limit) {
       copy $p.FullName $toDir
     }
   }
-
--- zip-tester.ps1
+++ zip-tester.ps1
@@ -1,3 +1,13 @@
+#v1.0.0
+<#
+功能：使用解压密码字典测试压缩文件。
+参数：
+  zipFile - 压缩文件路径。
+  dic - 解压密码字典文件。纯文本，每行一个解压密码。
+输出：如果字典中有正确的密码则输出，否则输出未找到密码。
+依赖：7zip，确保可以直接调用 7z 命令。
+#>
+
 [CmdletBinding()]
 param(
   [Parameter(Mandatory, Position=0)]
@@ -29,15 +39,9 @@ foreach($password in $pwds) {
     if($isFound) {
       break
     }
-    
-    #if(($length -gt 16) -and ($result[12] -ceq 'Everything is Ok')) {
-    #  Write-Host "password is $password"
-    #  $isFound = $true
-    #  break
-    #}
   }
 }
 
 if(-not $isFound) {
     Write-Host "password not found"
-  }
+ }
