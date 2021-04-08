5c944e9fc5de5619d9fc65cc531aa4e6ada4c1e8
Eric Zong <ericzonglu@126.com>
2021-04-09 00:49:51 +0800

    临时保存

--- iptools.ps1
+++ iptools.ps1
@@ -1,4 +1,5 @@
-function getGitHubIp {
+function getGitHubIp 
+{
     $html = Invoke-WebRequest -URI https://github.com.ipaddress.com/
     $pattern = '(?<=<ul class="comma-separated"><li>)(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
     $ip = [regex]::matches($html, $pattern).groups[0].value
@@ -6,19 +7,37 @@ function getGitHubIp {
     return $ip
 }
 
-function overrideHosts {
+Function Edit-Hosts
+{
+    Start-Process notepad.exe -Verb runas -ArgumentList $env:windir\System32\drivers\etc\hosts -Wait
+    ipconfig /flushdns | Out-Null
+}
+
+function overrideHosts 
+{
+    $gitHubIp = getGitHubIp
+    $githubConfig = "$gitHubIp`tgithub.com"
+
     $hostsPath = 'C:\Windows\System32\drivers\etc\hosts'
     $hostsText = Get-Content $hostsPath
-    $hosts = $hostsText -split '\r\n'
+    $hostsText = ($hostsText -replace "$githubConfig",'')
+    $hosts = $hostsText -split '`r`n'
 
-
-
+    $outString = ""
+    $firstConfig = $True
     foreach($line in $hosts) {
-        # Out-Host $line
         if(($line -match "^#") -or ($line -eq "")) {
-            Out-File -FilePath E:/hosts.txt -Append -InputObject $line
+            $outString += "$line`r`n"
         } else {
-            $ipDomain = ($line -split "\t")
+            if($firstConfig) {
+                $firstConfig = $False
+                $outString += "$githubConfig`r`n"
+            }
+            
+            $outString += "$line`r`n"
         }
     }
+
+    Set-Clipboard $outString
+    Edit-Hosts
 }
